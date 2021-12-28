51c78070216f6161a3d06bc12f02c4c36860b639
George Avsetsin <mail@sjors.ru>
2021-12-28 15:39:49 +0300

    test: fix fetch rpc test

--- packages/fetch/test/fetchRPC.test.ts
+++ packages/fetch/test/fetchRPC.test.ts
@@ -46,11 +46,13 @@ describe('fetchRPC', () => {
     expect(result).toBe(expected);
   });
 
-  // test('should combine an url string correctly', async () => {
-  //   const url = (chainId: CHAINS) => `https://example.com?chainId=${chainId}`;
-  //   await fetchRPC(CHAINS.Mainnet, { urls: [url] });
-  //
-  //   expect(fetch).toHaveBeenCalledTimes(1);
-  //   expect(fetch).toHaveBeenCalledWith(url(CHAINS.Mainnet), params);
-  // });
+  test('should combine an url string correctly', async () => {
+    const url = (chainId: CHAINS) => `https://example.com?chainId=${chainId}`;
+    mockFetch.mockReturnValue(Promise.resolve(new Response(expected)));
+
+    await fetchRPC(CHAINS.Mainnet, { urls: [url] });
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(url(CHAINS.Mainnet), params);
+  });
 });
