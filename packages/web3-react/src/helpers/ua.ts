67a838379295e4b1e767c3e2a7850df8193b0837
Alexander Khramov <alx.khramov@gmail.com>
2022-08-18 16:50:12 +0300

    fix(web3-react): improve metamask connection logic

--- packages/web3-react/src/helpers/ua.ts
+++ packages/web3-react/src/helpers/ua.ts
@@ -15,3 +15,8 @@ export const isMobileOrTablet = isMobile || isTablet;
 
 export const isFirefox = browser.name === 'Firefox';
 export const isEdge = browser.name === 'Edge';
+
+export const checkIfBraveBrowser = async (): Promise<boolean> =>
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  (navigator.brave && (await navigator.brave.isBrave())) || false;
